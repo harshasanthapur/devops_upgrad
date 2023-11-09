6f3d3a578a2cdd5e86eb140d94307fd37708f8c4
sriharsha <sriharsha.santhapur@zf.com>
2023-11-09 18:40:37 +0530

    Added shell scripts

---
+++ bash_scripting/condition_4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+names='Harsha Lalitha Havya'
+
+for x in $names
+do
+    echo $x
+done
+for x in {1..10}
+do
+    echo $x
+done
+for x in {1..10..2}
+do
+    echo $x
+done
--- bash_scripting/first_script.sh
+++ bash_scripting/first_script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-touch file.txt
+echo Hello World!
+key=Sriharsha
+echo $key
+echo $1
+echo $2
+echo $0 # file name itself
+echo $#
